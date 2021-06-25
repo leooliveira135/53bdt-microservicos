8c0f8bc93679241c791095937a0ff56918e9da69
Leonardo Oliveira <oliveiraleonardo61@gmail.com>
2021-06-24 21:35:26 -0300

    Atualização da api com o metodo getAlternative

--- api/api.py
+++ api/api.py
@@ -16,6 +16,23 @@ app.config['MYSQL_DATABASE_HOST'] = 'mysql'
 
 mysql.init_app(app)
 
+def jsonfyer(obj):
+
+	if isinstance(obj, (datetime,date)):
+		return obj.isoformat()
+
 @app.route("/Teste")
 def Teste():
 	return "Welcome to teste!"
+
+@app.route("/getAlternative")
+def getAlternative():
+	cursor = mysql.connect().cursor()
+	cursor.execute("SELECT * FROM 53bdt_db.spotify_alt_playlists_data")
+	r = [dict((cursor.description[i][0],value) for i, value in enumerate(row))
+			for row in cursor.fetchall()]
+	json_str = json.dumps(r, default=jsonfyer)
+	return json_str
+
+if __name__ == "__main__":
+	app.run(host='0.0.0.0', debug=True)
