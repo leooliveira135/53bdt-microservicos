30bf25ea027a02a687355416d6dd92193696f788
Leonardo Oliveira <oliveiraleonardo61@gmail.com>
2021-06-25 21:40:29 -0300

    Atualizações do ddl

--- mysql/spotify_ddl.sql
+++ mysql/spotify_ddl.sql
@@ -27,7 +27,7 @@ CREATE TABLE IF NOT EXISTS 53bdt_db.spotify_rock_playlists_data(
     time_signature integer
 );
 
-LOAD DATA INFILE 'archive/rock_music_data.csv'
+LOAD DATA INFILE 'rock_music_data.csv'
 INTO TABLE 53bdt_db.spotify_rock_playlists_data
 FIELDS TERMINATED BY ','
 LINES TERMINATED BY '\n'
@@ -58,7 +58,7 @@ CREATE TABLE IF NOT EXISTS 53bdt_db.spotify_pop_playlists_data(
     time_signature integer
 );
 
-LOAD DATA INFILE 'archive/pop_music_data.csv'
+LOAD DATA INFILE 'pop_music_data.csv'
 INTO TABLE 53bdt_db.spotify_pop_playlists_data
 FIELDS TERMINATED BY ','
 LINES TERMINATED BY '\n'
@@ -89,7 +89,7 @@ CREATE TABLE IF NOT EXISTS 53bdt_db.spotify_metal_playlists_data(
     time_signature integer
 );
 
-LOAD DATA INFILE 'archive/metal_music_data.csv'
+LOAD DATA INFILE 'metal_music_data.csv'
 INTO TABLE 53bdt_db.spotify_metal_playlists_data
 FIELDS TERMINATED BY ','
 LINES TERMINATED BY '\n'
@@ -120,7 +120,7 @@ CREATE TABLE IF NOT EXISTS 53bdt_db.spotify_indie_playlists_data(
     time_signature integer
 );
 
-LOAD DATA INFILE 'archive/indie_alt_music_data.csv'
+LOAD DATA INFILE 'indie_alt_music_data.csv'
 INTO TABLE 53bdt_db.spotify_indie_playlists_data
 FIELDS TERMINATED BY ','
 LINES TERMINATED BY '\n'
@@ -151,7 +151,7 @@ CREATE TABLE IF NOT EXISTS 53bdt_db.spotify_hiphop_playlists_data(
     time_signature integer
 );
 
-LOAD DATA INFILE 'archive/hiphop_music_data.csv'
+LOAD DATA INFILE 'hiphop_music_data.csv'
 INTO TABLE 53bdt_db.spotify_hiphop_playlists_data
 FIELDS TERMINATED BY ','
 LINES TERMINATED BY '\n'
@@ -182,7 +182,7 @@ CREATE TABLE IF NOT EXISTS 53bdt_db.spotify_blues_playlists_data(
     time_signature integer
 );
 
-LOAD DATA INFILE 'archive/blues_music_data.csv'
+LOAD DATA INFILE 'blues_music_data.csv'
 INTO TABLE 53bdt_db.spotify_blues_playlists_data
 FIELDS TERMINATED BY ','
 LINES TERMINATED BY '\n'
@@ -213,7 +213,7 @@ CREATE TABLE IF NOT EXISTS 53bdt_db.spotify_alt_playlists_data(
     time_signature integer
 );
 
-LOAD DATA INFILE 'archive/alternative_music_data.csv'
+LOAD DATA INFILE 'alternative_music_data.csv'
 INTO TABLE 53bdt_db.spotify_alt_playlists_data
 FIELDS TERMINATED BY ','
 LINES TERMINATED BY '\n'
